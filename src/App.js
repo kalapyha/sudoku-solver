2b1bf44b72d10685c2f6f21bca09633856ea178a
Nikolai Hapatnyukovsky <nikolaihapatnyukovsky@Nikolais-iMac.local>
2021-06-02 14:02:39 -0400

    loader added, styles updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import './App.css';
+import { useState } from 'react';
 import SudokuBoard from './components/SudokuBoard';
 import Button from './components/Button';
+import Loader from './components/Loader';
 import { defaultBaord_9by9, emptyBaord_9by9 } from './utils/sudoku_solver';
 
 function App() {
+	const [isLoading, setisLoading] = useState(false);
 	return (
 		<div className="app-wrapper">
 			<SudokuBoard board={defaultBaord_9by9} />
 			<Button />
-			<SudokuBoard board={emptyBaord_9by9} />
+			{isLoading ? <Loader /> : <SudokuBoard board={emptyBaord_9by9} />}
 		</div>
 	);
 }
